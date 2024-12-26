d65dce487ee6ac8caae8b9c07385c90de53b1f98
Sari <sari.solieman@gmail.com>
2024-12-27 00:31:27 +0200

    Added icons

--- src/components/weather.jsx
+++ src/components/weather.jsx
@@ -1,5 +1,6 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import axios from "axios";
+import { icons } from "../scripts/icons";
 
 const Weather = () => {
     const [city, setCity] = useState("");
@@ -9,13 +10,17 @@ const Weather = () => {
         try {
             const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${import.meta.env.VITE_APP_ID}`)
             const data = response.data;
+
+           
+            const icon = icons[data.weather[0].icon]
+
             setWeatherData({
                 humidity: data.main.humidity,
                 windSpeed: data.wind.speed,
                 temperature: Math.floor(data.main.temp),
                 location: data.name,
                 description: data.weather[0].description,
-                icon: data.weather[0].icon
+                icon: icon
             });
 
         } catch (error) {
@@ -25,14 +30,8 @@ const Weather = () => {
 
     const handleSubmit = (event) => {
         event.preventDefault();
-        fetchWeatherData();
+        fetchWeatherData(city);
     };
-
-    useEffect(() => {
-        fetchWeatherData('New York')
-        console.log(weatherData);
-
-    }, [])
 
     return (
         <>
@@ -47,16 +46,32 @@ const Weather = () => {
                     <button className="search-btn" type="submit">Get Weather</button>
                 </form>
                 {weatherData && (
-                    <div>
+                    <div className="weather-card">
                         <h2>{weatherData.location}</h2>
-                        <p>{weatherData.icon}</p>
-                        <p>{weatherData.description}</p>
-                        <p>Temperature: {weatherData.temperature} °C</p>
-                        <p>Wind Speed: {weatherData.windSpeed} km/h</p>
-                        <p>Humidity: {weatherData.humidity} %</p>
+                        <img src={weatherData.icon} className="weather-condition" />
+                        <div>{weatherData.description}</div>
+                        <div className="temperature">{weatherData.temperature}°C</div>
+                        <div className="condition-details">
+                            <div className="condition-details">
+                                <img src="/assets/wind.png" className="condition-img" />
+                                <div>
+                                    {weatherData.windSpeed} km/h
+                                    <br />
+                                    Wind Speed
+                                </div>
+                            </div>
+                            <div className="condition-details">
+                                <img src="/assets/humidity.png" className="condition-img" />
+                                <div>
+                                    {weatherData.humidity}%
+                                    <br />
+                                    Humidity
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 )}
-            </div>
+            </div >
         </>
     );
 };
---
+++ src/scripts/icons.js
@@ -0,0 +1,30 @@
+import sun from '/assets/sun.png';
+import moon from '/assets/moon.png';
+import cloudyDay from '/assets/cloudy-day.png';
+import cloudyNight from '/assets/cloudy-night.png';
+import cloud from '/assets/cloud.png';
+import rain from '/assets/rain.png';
+import snow from '/assets/snow.png';
+import thunderstorm from '/assets/thunderstorm.png';
+import mist from '/assets/mist.png'
+
+export const icons = {
+    '01d': sun,
+    '01n': moon,
+    '02d': cloudyDay,
+    '02n': cloudyNight,
+    '03d': cloud,
+    '03n': cloud,
+    '04d': cloud,
+    '04n': cloud,
+    '09d': rain,
+    '09n': rain,
+    '10d': rain,
+    '10n': rain,
+    '11d': thunderstorm,
+    '11n': thunderstorm,
+    '13d': snow,
+    '13n': snow,
+    '50d': mist,
+    '50n': mist,
+}
